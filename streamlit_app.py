bcd20ac74ab3fe3962c5a3c95f73c8a3dea0bb33
Abid Ramay <kasper_341@hotmail.com>
2023-09-23 15:54:50 -0700

    Update streamlit_app.py
    added smoothie menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title("My parents kitchen")
 
-streamlit.header("Breakfast Menu 🍔")
-streamlit.text("Omalate")
-streamlit.text("french toast")
-streamlit.text("Jam")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
