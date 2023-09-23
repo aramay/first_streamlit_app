2b4cb3abf7c55b80a7c4f19c55119d7a771eafb5
Abid Ramay <kasper_341@hotmail.com>
2023-09-23 16:12:45 -0700

    Update streamlit_app.py
    add multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,9 @@ st.text('Hard-Boiled Free-Range Egg')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# add pick list here
+st.multiselect("Pick some fruits ", list(fruits_list))
+
+# display the table
 st.dataframe(fruits_list)
