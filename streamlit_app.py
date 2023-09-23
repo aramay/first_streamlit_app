0aede746daf41e4bdbaa8893c942edefacd5512c
Abid Ramay <kasper_341@hotmail.com>
2023-09-23 15:58:13 -0700

    Update streamlit_app.py
    add pandas
    read data from a csv file
    display data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
-import streamlit
+import streamlit as st
+import pandas as pd
 
-streamlit.title("My parents kitchen")
+st.title("My parents kitchen")
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(fruits_list)
