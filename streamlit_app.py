5caf9355433edbfe414d40274f71bc2d134da1be
Abid Ramay <kasper_341@hotmail.com>
2023-09-24 13:59:06 -0700

    Update streamlit_app.py
    take user input - fruit choice.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,13 @@ fruits_to_show = fruits_list.loc[fruits_selected]
 # display the table
 st.dataframe(fruits_to_show)
 
+# Fruityvice Fruit Advice
 st.header('Fruityvice Fruit Advice')
 
-fruityvice_res = re.get('https://www.fruityvice.com/api/fruit/' + 'kiwi')
+fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+st.write('The user entered', fruit_choice)
+
+fruityvice_res = re.get('https://www.fruityvice.com/api/fruit/' + fruit_choice)
 
 #st.text(fruityvice_res.json())
 
